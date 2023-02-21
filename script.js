0b4405e53ad8f252489a2da43ee7022671c7887d
Benjamin Hove <benhove86@gmail.com>
2023-02-21 15:47:35 -0500

    Version update

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+
+email.addEventListener("input", (event) => {
+    if (email.validity.valid) {
+        emailError.textContent = "";
+        emailError.className = "error";
+    } else {
+        showError();
+    }
+
+});
+
+function showError() {
+    if (email.validity.valueMissing) {
+        emailError.textContent = "Please enter an email address"
+    } else if (email.validity.typeMismatch) {
+        emailError.textContent = "Please provide a valid email address"
+    }
+}
+
+
+form.addEventListener("submit", (event) => {
+    if (!email.validity.valid) {
+        showError();
+        event.preventDefault();
+    }   
+});
